6db7f1dfae5fa1f435837e58db47366eadb067eb
Michał Daniel <michal.daniel@makolab.com>
2019-10-25 13:38:50 +0200

    Loaded configuration file from specific location

--- config/src/main/java/org/edmcouncil/spec/fibo/config/configuration/loader/ConfigLoader.java
+++ config/src/main/java/org/edmcouncil/spec/fibo/config/configuration/loader/ConfigLoader.java
@@ -17,31 +17,31 @@ import org.xml.sax.SAXException;
  * @author Michał Daniel (michal.daniel@makolab.com)
  */
 public class ConfigLoader {
-  
+
   private static Logger logger = LoggerFactory.getLogger(ConfigLoader.class);
-  
+
 
   public Configuration loadWeaselConfiguration(Path weaselConfigFile) {
     Configuration configuration = new WeaselConfiguration();
-    
+
     SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
     try {
         File configFile= weaselConfigFile.toFile();
         if(!configFile.exists()){
-          logger.debug("Configuration file not exist, use default empty configuration.");
-          return configuration;
-        }
-        SAXParser saxParser = saxParserFactory.newSAXParser();
-        WeaselConfigurationHandler handler = new WeaselConfigurationHandler();
-        saxParser.parse(configFile, handler);
-        
-        configuration = handler.getConfiguration();
-       
+        logger.debug("Configuration file not exist, use default empty configuration.");
+        return configuration;
+      }
+      SAXParser saxParser = saxParserFactory.newSAXParser();
+      WeaselConfigurationHandler handler = new WeaselConfigurationHandler();
+      saxParser.parse(configFile, handler);
+
+      configuration = handler.getConfiguration();
+
         
     } catch (ParserConfigurationException | SAXException | IOException e) {
-        logger.error("Exception while loading configuration: {}", e.getMessage());
+      logger.error("Exception while loading configuration: {}", e.getMessage());
     }
-      
+
 
     return configuration;
   }
