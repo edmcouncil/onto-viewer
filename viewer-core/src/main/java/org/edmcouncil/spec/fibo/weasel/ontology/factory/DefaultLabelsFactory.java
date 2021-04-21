6ac3c20e9485e283e0c90ef7d44dc4777b6f93bc
patrycjamia <patrycjamiazek17@gmail.com>
2021-04-21 15:04:40 +0200

    add usage info for classes and object property
    Signed-off-by: patrycjamia <patrycjamiazek17@gmail.com>

--- viewer-core/src/main/java/org/edmcouncil/spec/fibo/weasel/ontology/factory/DefaultLabelsFactory.java
+++ viewer-core/src/main/java/org/edmcouncil/spec/fibo/weasel/ontology/factory/DefaultLabelsFactory.java
@@ -5,6 +5,7 @@ import org.edmcouncil.spec.fibo.weasel.ontology.data.label.vocabulary.DefaultApp
 
 /**
  * Factory create default application labels for embeded functions and ontology resourcess.
+ *
  * @author Michał Daniel (michal.daniel@makolab.com)
  */
 public class DefaultLabelsFactory {
@@ -12,44 +13,46 @@ public class DefaultLabelsFactory {
   /**
    * @return complete instance of DefaultAppLabels
    */
-  public static DefaultAppLabels createDefaultAppLabels(){
+  public static DefaultAppLabels createDefaultAppLabels() {
     DefaultAppLabels result = new DefaultAppLabels();
     String iri = ViewerIdentifierFactory.createId(ViewerIdentifierFactory.Type.external, ViewerIdentifierFactory.Element.clazz);
     result.addLabel(iri, DefaultAppLabels.DEF_EXT_CLASS);
-    
-  
+
     iri = ViewerIdentifierFactory.createId(ViewerIdentifierFactory.Type.internal, ViewerIdentifierFactory.Element.clazz);
     result.addLabel(iri, DefaultAppLabels.DEF_INT_CLASS);
-    
+
     iri = ViewerIdentifierFactory.createId(ViewerIdentifierFactory.Type.external, ViewerIdentifierFactory.Element.dataProperty);
     result.addLabel(iri, DefaultAppLabels.DEF_EXT_DATA_PROPERTIES);
-    
+
     iri = ViewerIdentifierFactory.createId(ViewerIdentifierFactory.Type.internal, ViewerIdentifierFactory.Element.dataProperty);
     result.addLabel(iri, DefaultAppLabels.DEF_INT_DATA_PROPERTIES);
-    
+
     iri = ViewerIdentifierFactory.createId(ViewerIdentifierFactory.Type.external, ViewerIdentifierFactory.Element.objectProperty);
     result.addLabel(iri, DefaultAppLabels.DEF_EXT_OBJECT_PROPERTIES);
-    
+
     iri = ViewerIdentifierFactory.createId(ViewerIdentifierFactory.Type.internal, ViewerIdentifierFactory.Element.objectProperty);
     result.addLabel(iri, DefaultAppLabels.DEF_INT_OBJECT_PROPERTIES);
-    
+
     iri = ViewerIdentifierFactory.createId(ViewerIdentifierFactory.Type.external, ViewerIdentifierFactory.Element.instance);
     result.addLabel(iri, DefaultAppLabels.DEF_EXT_INSTANCES);
-    
+
     iri = ViewerIdentifierFactory.createId(ViewerIdentifierFactory.Type.internal, ViewerIdentifierFactory.Element.instance);
     result.addLabel(iri, DefaultAppLabels.DEF_INT_INSTANCES);
-   
+
     iri = ViewerIdentifierFactory.createId(ViewerIdentifierFactory.Type.function, WeaselOwlType.DIRECT_SUBCLASSES.toString().toLowerCase());
     result.addLabel(iri, DefaultAppLabels.DEF_DIRECT_SUBCLASSES);
-    
+
     iri = ViewerIdentifierFactory.createId(ViewerIdentifierFactory.Type.function, WeaselOwlType.INSTANCES.name().toLowerCase());
     result.addLabel(iri, DefaultAppLabels.DEF_INSTANCES);
-    
+
     iri = ViewerIdentifierFactory.createId(ViewerIdentifierFactory.Type.function, WeaselOwlType.ANONYMOUS_ANCESTOR.name().toLowerCase());
     result.addLabel(iri, DefaultAppLabels.DEF_ANONYMOUS_ANCESTOR);
-    
+
+    iri = ViewerIdentifierFactory.createId(ViewerIdentifierFactory.Type.function, WeaselOwlType.USAGE_CLASSES.toString().toLowerCase());
+    result.addLabel(iri, DefaultAppLabels.DEF_USAGE_CLASSES);
+
     return result;
-    
+
   }
-  
+
 }
