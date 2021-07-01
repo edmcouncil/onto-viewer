503798559899183ae91e98a088a39e05d1340ed4
patrycjamia <patrycjamiazek17@gmail.com>
2021-07-01 09:03:00 +0200

    grouped inherited restrictions by the class they are coming from
    Signed-off-by: patrycjamia <patrycjamiazek17@gmail.com>

--- viewer-core/src/main/java/org/edmcouncil/spec/fibo/weasel/model/property/OwlAxiomPropertyValue.java
+++ viewer-core/src/main/java/org/edmcouncil/spec/fibo/weasel/model/property/OwlAxiomPropertyValue.java
@@ -2,6 +2,7 @@ package org.edmcouncil.spec.fibo.weasel.model.property;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author Michał Daniel (michal.daniel@makolab.com)
@@ -48,4 +49,38 @@ public class OwlAxiomPropertyValue extends PropertyValueAbstract<String> {
     return fullRenderedString == null ? entityMaping.toString() : fullRenderedString;
   }
 
+//  @Override
+//  public int hashCode() {
+//    int hash = 7;
+//   // hash = 73 * hash + Objects.hashCode(this.entityMaping);
+//    hash = 73 * hash + Objects.hashCode(this.fullRenderedString);
+//    return hash;
+//  }
+//
+//  @Override
+//  public boolean equals(Object obj) {
+//    if (this == obj) {
+//      return true;
+//    }
+//    if (obj == null) {
+//      return false;
+//    }
+//    if (getClass() != obj.getClass()) {
+//      return false;
+//    }
+//    final OwlAxiomPropertyValue other = (OwlAxiomPropertyValue) obj;
+////    if (this.lastId != other.lastId) {
+////      return false;
+////    }
+//    if (!Objects.equals(this.fullRenderedString, other.fullRenderedString)) {
+//      return false;
+//    }
+////    if (!Objects.equals(this.entityMaping, other.entityMaping)) {
+////      return false;
+////    }
+//    return true;
+//  }
+
+  
+  
 }
