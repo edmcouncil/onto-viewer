35974247cc2bc1df586bec18897a2faa8cd47568
Patrycja Miazek <patrycjamiazek17@gmail.com>
2021-09-27 14:59:31 +0200

    103 modify the actulator prefix and add a custom (#105)
    * modify the actulator prefix and add a custom
    
    Signed-off-by: patrycjamia <patrycjamiazek17@gmail.com>

---
+++ onto-viewer-web-app/src/main/java/org/edmcouncil/spec/ontoviewer/webapp/actuator/CustomHealthIndicator.java
@@ -0,0 +1,48 @@
+package org.edmcouncil.spec.ontoviewer.webapp.actuator;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.edmcouncil.spec.ontoviewer.core.ontology.updater.UpdateBlocker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Patrycja Miazek (patrycja.miazek@makolab.com)
+ */
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+
+  private final UpdateBlocker updateBlocker;
+
+  public CustomHealthIndicator(UpdateBlocker updateBlocker) {
+    this.updateBlocker = updateBlocker;
+  }
+
+  @Override
+  public Health getHealth(boolean includeDetails) {
+    return check();
+  }
+
+  @Override
+  public Health health() {
+    return check();
+  }
+
+  private Health check() {
+    Map<String, Boolean> details = new HashMap<>();
+    try {
+      details.put(HealthDetailsField.INITIALIZATION_DONE.name(), updateBlocker.isInitializeAppDone());
+      details.put(HealthDetailsField.UPDATE_ONTOLOGY_IN_PROGRESS.name(), updateBlocker.isUpdateNow());
+      details.put(HealthDetailsField.BLOCKED.name(), updateBlocker.isBlocked());
+    } catch (Exception e) {
+      return Health.down(e).build();
+    }
+    return Health.up().withDetails(details).build();
+  }
+
+  public UpdateBlocker getUpdateBlocker() {
+    return updateBlocker;
+  }
+}
---
+++ onto-viewer-web-app/src/main/java/org/edmcouncil/spec/ontoviewer/webapp/actuator/HealthDetailsField.java
@@ -0,0 +1,8 @@
+package org.edmcouncil.spec.ontoviewer.webapp.actuator;
+
+/**
+ * @author Patrycja Miazek (patrycja.miazek@makolab.com)
+ */
+public enum HealthDetailsField {
+  INITIALIZATION_DONE, UPDATE_ONTOLOGY_IN_PROGRESS, BLOCKED
+}
