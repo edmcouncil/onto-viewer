06b4a9223a7d019344497b01f67805195915cf7e
Patrycja Miazek <patrycja.miazek@makolab.com>
2022-01-26 14:40:02 +0100

    Added dataType
    Signed-off-by: Patrycja Miazek <patrycja.miazek@makolab.com>

--- onto-viewer-core/src/main/java/org/edmcouncil/spec/ontoviewer/core/ontology/data/OwlDataHandler.java
+++ onto-viewer-core/src/main/java/org/edmcouncil/spec/ontoviewer/core/ontology/data/OwlDataHandler.java
@@ -1149,7 +1149,7 @@ public class OwlDataHandler {
   public OwlListDetails handleParticularDatatype(IRI iri, OWLOntology ontology) {
     OwlListDetails resultDetails = new OwlListDetails();
 
-    var entityEntry = entitiesCacheService.getEntityEntry(iri, OwlType.DATATYPE);
+    var entityEntry = entitiesCacheService.getEntityEntry(iri, OwlType.DATA_TYPE);
 
     try {
       if (entityEntry.isPresent()) {
