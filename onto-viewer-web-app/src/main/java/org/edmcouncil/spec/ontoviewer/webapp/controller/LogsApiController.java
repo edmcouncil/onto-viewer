736b0b2748c21257b193781d6905b1b6c15afcb1
Patrycja Miazek <patrycja.miazek@makolab.com>
2022-02-04 08:45:49 +0100

    Fixed misleading documentation
    Signed-off-by: Patrycja Miazek <patrycja.miazek@makolab.com>

--- onto-viewer-web-app/src/main/java/org/edmcouncil/spec/ontoviewer/webapp/controller/LogsApiController.java
+++ onto-viewer-web-app/src/main/java/org/edmcouncil/spec/ontoviewer/webapp/controller/LogsApiController.java
@@ -24,21 +24,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping(value = {"/api/logs"})
 public class LogsApiController {
-  
+
   private static final Logger LOG = LoggerFactory.getLogger(LogsApiController.class);
   private final ApiKeyService keyService;
   private final LogsService logsService;
-  
+
   public LogsApiController(ApiKeyService keyService, LogsService logsService) {
     this.keyService = keyService;
     this.logsService = logsService;
   }
-  
+
   @GetMapping
   public ResponseEntity getLogs(@RequestHeader(name = "X-API-Key", required = false) String apiKeyHeader,
-          @RequestParam(value = "ApiKey", required = false) String apiKeyParam,
-          @RequestParam(value = "date", required = false) Optional<String> date,
-          @RequestHeader(value = "Accept", required = true) String acceptHeader) throws IOException {
+      @RequestParam(value = "ApiKey", required = false) String apiKeyParam,
+      @RequestParam(value = "date", required = false) Optional<String> date,
+      @RequestHeader(value = "Accept", required = true) String acceptHeader) throws IOException {
     if (!acceptHeader.contains(MediaType.TEXT_PLAIN_VALUE)) {
       return ResponseEntity.badRequest().body("Incorrect or missing header. `Accept: " + acceptHeader + "'");
     }
@@ -48,15 +48,15 @@ public class LogsApiController {
     } else if (apiKeyParam != null) {
       key = apiKeyParam;
     }
-    
+
     if (!keyService.validateApiKey(key)) {
       LOG.debug(API_KEY_NOT_VALID_MESSAGE);
       return ResponseEntity.badRequest().body(
-              new ErrorResponse(API_KEY_NOT_VALID_MESSAGE, null));
+          new ErrorResponse(API_KEY_NOT_VALID_MESSAGE, null));
     }
     List<String> logs = logsService.getLogs(date.orElse(null));
-    
+
     return ResponseEntity.ok(String.join("\n", logs));
   }
-  
+
 }
