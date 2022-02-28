e888671eadd8ba7821b439fb403090f4b4b4895c
Patrycja Miazek <patrycja.miazek@makolab.com>
2022-02-28 12:27:30 +0100

    corrections
    Signed-off-by: Patrycja Miazek <patrycja.miazek@makolab.com>

--- onto-viewer-web-app/src/main/java/org/edmcouncil/spec/ontoviewer/webapp/controller/MissingImportsController.java
+++ onto-viewer-web-app/src/main/java/org/edmcouncil/spec/ontoviewer/webapp/controller/MissingImportsController.java
@@ -1,37 +1,34 @@
 package org.edmcouncil.spec.ontoviewer.webapp.controller;
 
+import java.util.Set;
 import org.edmcouncil.spec.ontoviewer.core.ontology.OntologyManager;
-import org.edmcouncil.spec.ontoviewer.core.ontology.loader.listener.MissingImportListenerImpl;
+import org.edmcouncil.spec.ontoviewer.core.ontology.loader.listener.MissingImport;
 import org.edmcouncil.spec.ontoviewer.webapp.boot.UpdateBlocker;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author patrycja.miazek (patrycja.miazek@makolab.com)
  */
 
-@Controller
-@RequestMapping(value = {"/api/missingImports"})
-public class MissingImportsController {
+@RestController
+@RequestMapping("/api/missingImports")
+public class MissingImportsController extends BaseController {
 
-  private static final Logger LOG = LoggerFactory.getLogger(MissingImportsController.class);
   @Autowired
   private OntologyManager ontologyManager;
-  @Autowired
-  private UpdateBlocker blocker;
-  
-   @GetMapping
-  public ResponseEntity getMissingImportsAsJson() {
-    if (!blocker.isInitializeAppDone()) {
-      LOG.debug("Application initialization has not completed");
-      return new ResponseEntity<>("503 Service Unavailable", HttpStatus.SERVICE_UNAVAILABLE);
-    }
-    return ResponseEntity.ok(ontologyManager.getMissingImports());    
+
+  public MissingImportsController(OntologyManager ontologyManager, UpdateBlocker updateBlocker) {
+    super(updateBlocker);
+    this.ontologyManager = ontologyManager;
+  }
+
+  @GetMapping
+  public ResponseEntity<Set<MissingImport>> getMissingImportsAsJson() {
+    checkIfApplicationIsReady();
+    return ResponseEntity.ok(ontologyManager.getMissingImports());
   }
 }
