0a7dec2b13d6539f8be752e9443508db0dbec12d
Patrycja Miazek <patrycja.miazek@makolab.com>
2022-05-20 14:37:30 +0200

    masking headers
    Signed-off-by: Patrycja Miazek <patrycja.miazek@makolab.com>

--- onto-viewer-web-app/src/main/java/org/edmcouncil/spec/ontoviewer/webapp/configuration/RequestLoggingFilterConfig.java
+++ onto-viewer-web-app/src/main/java/org/edmcouncil/spec/ontoviewer/webapp/configuration/RequestLoggingFilterConfig.java
@@ -1,5 +1,6 @@
 package org.edmcouncil.spec.ontoviewer.webapp.configuration;
 
+import java.util.function.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
@@ -15,6 +16,10 @@ public class RequestLoggingFilterConfig {
     loggingFilter.setIncludeQueryString(true);
     loggingFilter.setIncludePayload(true);
     loggingFilter.setMaxPayloadLength(10000);
+    loggingFilter.setHeaderPredicate(header -> !header.toLowerCase().equals("x-api-key"));
+    loggingFilter.setHeaderPredicate(header -> !header.toLowerCase().equals("authorization"));
+    loggingFilter.setHeaderPredicate(header -> !header.toLowerCase().equals("proxy-authorization"));
+
     return loggingFilter;
   }
 }
