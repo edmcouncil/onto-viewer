188719964f0b0bba73031e361411c20763fcbf8c
Patrycja Miazek <patrycja.miazek@makolab.com>
2022-07-26 11:52:44 +0200

    fix
    Signed-off-by: Patrycja Miazek <patrycja.miazek@makolab.com>

--- onto-viewer-core/src/main/java/org/edmcouncil/spec/ontoviewer/core/comparator/ComparatorWithAlphabeticalOrder.java
+++ onto-viewer-core/src/main/java/org/edmcouncil/spec/ontoviewer/core/comparator/ComparatorWithAlphabeticalOrder.java
@@ -23,14 +23,16 @@ public class ComparatorWithAlphabeticalOrder {
       }
       String txt1 = obj1.toString().toLowerCase();
       String txt2 = obj2.toString().toLowerCase();
-      // TODO: This is never true; fix or remove it
-      if (obj1.getClass().getName().equals(obj2.getClass().getName().equals("OwlAxiomPropertyValue"))) {
-        txt1 = ((OwlAxiomPropertyValue)obj1).getFullRenderedString().toLowerCase();
-        txt2 = ((OwlAxiomPropertyValue)obj2).getFullRenderedString().toLowerCase();
+
+      if (obj1.getClass().getName().equals("OwlAxiomPropertyValue")
+          && obj2.getClass().getName().equals(obj2.getClass().getName())) {
+        txt1 = ((OwlAxiomPropertyValue) obj1).getFullRenderedString().toLowerCase();
+        txt2 = ((OwlAxiomPropertyValue) obj2).getFullRenderedString().toLowerCase();
       }
-      if (obj1.getClass().getName().equals(obj2.getClass().getName().equals("OwlListElementIndividualProperty"))) {
-        txt1 = ((OwlListElementIndividualProperty)obj1).getValue().getLabel().toLowerCase();
-        txt2 = ((OwlListElementIndividualProperty)obj2).getValue().getLabel().toLowerCase();
+      if (obj1.getClass().getName().equals("OwlListElementIndividualProperty")
+          && obj2.getClass().getName().equals(obj2.getClass().getName())) {
+        txt1 = ((OwlListElementIndividualProperty) obj1).getValue().getLabel().toLowerCase();
+        txt2 = ((OwlListElementIndividualProperty) obj2).getValue().getLabel().toLowerCase();
       }
       return txt1.compareTo(txt2);
 
