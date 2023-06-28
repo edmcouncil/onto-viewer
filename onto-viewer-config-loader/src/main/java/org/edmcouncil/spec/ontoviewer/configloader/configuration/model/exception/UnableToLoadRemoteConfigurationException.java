158822669c692427e1aaf3b0f365377e8a95e0cf
dasawanaka <michal.19.daniel.96@gmail.com>
2023-06-28 07:19:48 +0200

    Remove class duplicate
    Signed-off-by: dasawanaka <michal.19.daniel.96@gmail.com>

--- onto-viewer-config-loader/src/main/java/org/edmcouncil/spec/ontoviewer/configloader/configuration/model/exception/UnableToLoadRemoteConfigurationException.java
+++
@@ -1,9 +0,0 @@
-package org.edmcouncil.spec.ontoviewer.configloader.configuration.model.exception;
-
-public class UnableToLoadRemoteConfigurationException extends Exception {
-
-
-  public UnableToLoadRemoteConfigurationException(String message, Throwable cause) {
-    super(message, cause);
-  }
-}
